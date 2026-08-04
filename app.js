f68d307882a61916aedbc09ff1692a1151ffa338
Karan Mittal <km8529680@gmail.com>
2026-08-04 06:50:24 +0530

    added authorization to listings edit and delete button by comparing currUser._id and listing.owner._id

--- app.js
+++ app.js
@@ -98,6 +98,7 @@ app.use((req, res, next) =>{
 
 
 
+
 // app.get("/demouser" ,async(req, res) =>{
 //     let fakeUser = new User({
 //         email : "karan@google.com",
@@ -111,7 +112,7 @@ app.use((req, res, next) =>{
 
 
 
-//* ROUTERS
+//* ROUTERS  
 app.use("/listings" , listingRouter);
 app.use("/listings/:id/reviews" , reviewRouter);
 app.use("/" , userRouter);
